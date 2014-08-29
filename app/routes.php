15ca8a630d3c6699d16aa350c09e5742edfefc9f
Sudhi <sid4it@gmail.com>
2014-08-28 19:43:10 -0500

    minor code.

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,12 @@
 |
 */
 
-Route::get('/', function()
+/*Route::get('/', function()
 {
 	return View::make('hello');
-});
+});*/
+
+Route::get('/', array(
+        'as' => 'indexDashboard',
+        'uses' => 'MrJuliuss\Syntara\Controllers\DashboardController@getIndex')
+    );
